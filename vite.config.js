d7147555e16658840d6c7a8447a2e8424d522c55
Nishit Bayen <65007263+nishit0072e@users.noreply.github.com>
2025-11-25 20:49:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
-.
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Change 'repo-name' to your actual GitHub repository name if deploying to GitHub Pages
+  // e.g., base: '/iir-filter-designer/'
+  base: './', 
+})
